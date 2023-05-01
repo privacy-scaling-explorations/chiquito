a870d28a61235157170141bf841417da41df19b4
Steve Wang <qwang97@wharton.upenn.edu>
2023-05-01 19:19:55 +0800

    moved all api annotations to code files in order to use cargo docs; created docs folder from cargo docs; deployed on github pages using fork; will deploy using main once approved; this pull request only contains api documentation related edits

---
+++ docs/chiquito/compiler/cell_manager/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["Placement",""],["SignalPlacement",""],["SingleRowCellManager",""],["StepPlacement",""]],"trait":[["CellManager",""]]};
---
+++ docs/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"chiquito":["",[["ast",[],["expr.rs"]],["backend",[],["halo2.rs","mod.rs"]],["compiler",[],["cell_manager.rs","step_selector.rs"]],["dsl",[],["cb.rs"]]],["ast.rs","compiler.rs","dsl.rs","ir.rs","lib.rs","stdlib.rs","util.rs"]]\
+}');
+createSourceSidebar();
