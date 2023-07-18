c54495cf3c03006f4913abc8c69c054bce55cdf9
Leo Lara <leo@leolara.me>
2023-07-18 07:16:20 +0000

    Remove unused file

--- src/compiler/linker.rs
+++
@@ -1,9 +0,0 @@
-use crate::ir::{sc::SuperCircuit, Circuit};
-
-pub struct Linker {}
-
-impl Linker {
-    pub fn link<F>(units: Vec<Circuit<F>>) -> SuperCircuit<F> {
-        let sc = SuperCircuit::default();
-    }
-}
