c71ed19d9134fddce0af346adcff9d44d3628257
lancenonce <lancendavis47@gmail.com>
2023-07-24 20:28:23 -0400

    working circuit

--- examples/forth.rs
+++ examples/forth.rs
@@ -1,12 +1,5 @@
 use chiquito::{
-    ast::expr::*,
-    backend::halo2::{chiquito2Halo2, ChiquitoHalo2Circuit}, /* compiles to
-                                                             * Chiquito Halo2
-                                                             * backend,
-                                                             * which can be
-                                                             * integrated into
-                                                             * Halo2
-                                                             * circuit */
+    backend::halo2::{chiquito2Halo2, ChiquitoHalo2Circuit}, 
     compiler::{
         cell_manager::SingleRowCellManager, // input for constructing the compiler
         step_selector::SimpleStepSelectorBuilder, // input for constructing the compiler
@@ -18,118 +11,115 @@ use chiquito::{
     },
     ir::{assigments::AssigmentGenerator, Circuit}, // compiled circuit type
 };
-use halo2curves::{ff::Field, bn256::Fr};
+use halo2_proofs::dev::MockProver;
+use halo2curves::{bn256::Fr, ff::Field};
+use std::{cell::RefCell, hash::Hash, rc::Rc};
 
 // Generic types:
-// 1) (F, F) are the elements that are in the stack; the stack is made up of two signals with many steps so each basic operation consumes 1 step
-// 3) F for the result of the operation
-fn forth_circuit<F: Field + From<u64>>() -> (Circuit<F>, Option<AssigmentGenerator<F, ()>>) {
+// (F, F, F) are the elements that are in the stack along with the result of the operation
+fn forth_circuit<F: Field + From<u64> + Hash>() -> (Circuit<F>, Option<AssigmentGenerator<F, ()>>) {
     // PLONKish table for the FORTH stack:
     // | s1 | s2  | r |
-    // |  1 |  1  | 2 |
-    // |  3 |  2  | 6 |
-    // |  4 |  2  | 2 |
+    // |  1 |  1  | 2 | <- where step type is add
+    // |  3 |  2  | 6 | <- where step type is mul
+    // |  4 |  2  | 2 | <- where step type is sub
 
-    let forth = circuit::<(F, F), F>("FORTH", |ctx| {
-
+    let forth = circuit::<F, (), (F, F, F), _>("FORTH", |ctx| {
         // declare signals
-        let s1 = ctx.forward("s1");
-        let s2 = ctx.forward("s2");
-        let r = ctx.forward("r");
+        let s1 = Rc::new(RefCell::new(ctx.forward("s1")));
+        let s2 = Rc::new(RefCell::new(ctx.forward("s2")));
+        let r = Rc::new(RefCell::new(ctx.forward("r")));
 
-        // initialize steps
-        let add_step = ctx.step_type("add");
-        let sub_step = ctx.step_type("sub");
-        let mul_step = ctx.step_type("mul");
-        let div_step = ctx.step_type("div");
+        ctx.pragma_num_steps(3);
 
-        // first step type: Since it's a general VM, do we need an ordering to the steps?
-        ctx.pragma_first_step(add_step);
-        // last step type
-        ctx.pragma_last_step(div_step);
-        // total number of steps: Should this be arbitrary?
-        ctx.pragma_num_steps(4);
-
-        // addition operation step
         let add_step = ctx.step_type_def("add", |ctx| {
+            let s1_cloned = s1.clone();
+            let s2_cloned = s2.clone();
+            let r_cloned = r.clone();
+
             ctx.setup(move |ctx| {
-                ctx.constr(eq(s1 + s2, r));
+                ctx.constr(eq(
+                    *s1_cloned.borrow() + *s2_cloned.borrow(),
+                    *r_cloned.borrow(),
+                ));
             });
 
+            let s1_cloned = s1.clone();
+            let s2_cloned = s2.clone();
+            let r_cloned = r.clone();
+
             ctx.wg(move |ctx, (s1_value, s2_value, r_value): (F, F, F)| {
-                println!("add step: s1: {}, s2: {}, r: {}", s1_value, s2_value, r_value);
-                ctx.assign(s1, s1_value);
-                ctx.assign(s2, s2_value);
-                ctx.assign(r, r_value);
+                ctx.assign(*s1_cloned.borrow(), s1_value);
+                ctx.assign(*s2_cloned.borrow(), s2_value);
+                ctx.assign(*r_cloned.borrow(), r_value);
             })
         });
 
-        // subtraction operation step
         let sub_step = ctx.step_type_def("sub", |ctx| {
+            let s1_cloned = s1.clone();
+            let s2_cloned = s2.clone();
+            let r_cloned = r.clone();
+
             ctx.setup(move |ctx| {
-                ctx.constr(eq(s1 - s2, r));
+                ctx.constr(eq(
+                    *s1_cloned.borrow() - *s2_cloned.borrow(),
+                    *r_cloned.borrow(),
+                ));
             });
+
+            let s1_cloned = s1.clone();
+            let s2_cloned = s2.clone();
+            let r_cloned = r.clone();
 
             ctx.wg(move |ctx, (s1_value, s2_value, r_value): (F, F, F)| {
-                println!("sub step: s1: {}, s2: {}, r: {}", s1_value, s2_value, r_value);
-                ctx.assign(s1, s1_value);
-                ctx.assign(s2, s2_value);
-                ctx.assign(r, r_value);
+                ctx.assign(*s1_cloned.borrow(), s1_value);
+                ctx.assign(*s2_cloned.borrow(), s2_value);
+                ctx.assign(*r_cloned.borrow(), r_value);
             })
         });
 
-        // multiplication operation step
         let mul_step = ctx.step_type_def("mul", |ctx| {
+            let s1_cloned = s1.clone();
+            let s2_cloned = s2.clone();
+            let r_cloned = r.clone();
+
             ctx.setup(move |ctx| {
-                ctx.constr(eq(s1 * s2, r));
+                ctx.constr(eq(
+                    *s1_cloned.borrow() * *s2_cloned.borrow(),
+                    *r_cloned.borrow(),
+                ));
             });
 
-            ctx.wg(|ctx, (s1_result, s2_result, r_result): (F, F, F)| {
-                println!("mul step: s1: {}, s2: {}, r: {}", s1_result, s2_result, r_result);
-                ctx.assign(s1, s1_result);
-                ctx.assign(s2, s2_result);
-                ctx.assign(r, r_result);
-            })
-        });
+            let s1_cloned = s1.clone();
+            let s2_cloned = s2.clone();
+            let r_cloned = r.clone();
 
-        // division operation step
-        let div_step = ctx.step_type_def("div", |ctx| {
-            ctx.setup(move |ctx| {
-                ctx.constr(eq(s1 / s2, r));
-            });
-
-            ctx.wg(|ctx, (s1_result, s2_result, r_result): (F, F, F)| {
-                println!("div step: s1: {}, s2: {}, r: {}", s1_result, s2_result, r_result);
-                ctx.assign(s1, s1_result);
-                ctx.assign(s2, s2_result);
-                ctx.assign(r, r_result);
+            ctx.wg(move |ctx, (s1_value, s2_value, r_value): (F, F, F)| {
+                ctx.assign(*s1_cloned.borrow(), s1_value);
+                ctx.assign(*s2_cloned.borrow(), s2_value);
+                ctx.assign(*r_cloned.borrow(), r_value);
             })
         });
 
         ctx.trace(move |ctx: _, _| {
-            // Here we fill the circuit with the constraint 1 + 1 = 2
-            ctx.add(&add_step, (1, 1, 2));
+            // 1 + 1 = 2
+            ctx.add(&add_step, (F::from(1u64), F::from(1u64), F::from(2u64)));
             // 5 - 4 = 1
-            ctx.add(&sub_step, (5, 4, 1));
+            ctx.add(&sub_step, (F::from(5u64), F::from(4u64), F::from(1u64)));
             // 2 * 3 = 6
-            ctx.add(&mul_step, (2, 3, 6));
-            // 4 / 2 = 2
-            ctx.add(&div_step, (4, 2, 2));
+            ctx.add(&mul_step, (F::from(2u64), F::from(3u64), F::from(6u64)));
         });
     });
 
     let compiler = Compiler::new(SingleRowCellManager {}, SimpleStepSelectorBuilder {});
 
     compiler.compile(&forth)
-
 }
 
 fn main() {
     let (chiquito, wit_gen) = forth_circuit::<Fr>();
     let compiled = chiquito2Halo2(chiquito);
-    // passing 0 as the TraceArgs argument for g.generate as somewhat of a placeholder since TraceArgs aren't used in this example
-    let circuit = ChiquitoHalo2Circuit::new(compiled, wit_gen.map(|g| g.generate(0)));
-    println!("Compiled circuit: {:?}", circuit);
+    let circuit = ChiquitoHalo2Circuit::new(compiled, wit_gen.map(|g| g.generate(())));
 
     let prover = MockProver::<Fr>::run(7, &circuit, Vec::new()).unwrap();
 
