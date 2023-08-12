7f0eda5048a9d069f345a0ba8493a29c3187a8c8
leolara <leolara@users.noreply.github.com>
2023-08-12 12:05:17 +0000

    deploy: 98fc7f1b940c0be7d90f066b00c3714f2ce301ea

--- docs/chiquito/frontend/sidebar-items.js
+++ docs/chiquito/frontend/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["pychiquito",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["dsl",""],["pychiquito",""]]};
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"chiquito":["",[["ast",[["expr",[],["query.rs"]]],["expr.rs"]],["backend",[],["halo2.rs","mod.rs","plaf.rs"]],["compiler",[],["cell_manager.rs","step_selector.rs","unit.rs"]],["dsl",[],["cb.rs","lb.rs","sc.rs"]],["frontend",[],["mod.rs","pychiquito.rs"]],["ir",[],["assignments.rs","sc.rs"]]],["ast.rs","compiler.rs","dsl.rs","errors.rs","ir.rs","lib.rs","stdlib.rs","util.rs","wit_gen.rs"]]\
+"chiquito":["",[["ast",[["expr",[],["mod.rs","query.rs"]]],["mod.rs"]],["frontend",[["dsl",[],["cb.rs","lb.rs","mod.rs","sc.rs"]]],["mod.rs","pychiquito.rs"]],["plonkish",[["backend",[],["halo2.rs","mod.rs","plaf.rs"]],["compiler",[],["cell_manager.rs","mod.rs","step_selector.rs","unit.rs"]],["ir",[],["assignments.rs","mod.rs","sc.rs"]]],["mod.rs"]]],["lib.rs","stdlib.rs","util.rs","wit_gen.rs"]]\
 }');
 createSourceSidebar();
