4fe6e176937bce078f61d49a3b3d95e81097fb7a
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2023-08-30 06:53:39 -0400

    Finalize fibo tutorials (#115)
    Ready for review.
    - All Fibo tutorials now work (through chapter 5)
    - Added padding related features in Python frontend to support new
    tutorial chapters
    - Published latest Chiquito Python package `0.1.2023082800` to PyPI

--- examples/fibonacci.py
+++ examples/fibonacci.py
@@ -1,62 +1,87 @@
-from __future__ import annotations
-from typing import Tuple
-
 from chiquito.dsl import Circuit, StepType
 from chiquito.cb import eq
-from chiquito.query import Queriable
 from chiquito.util import F
+from chiquito.chiquito_ast import Last
 
 
-class Fibonacci(Circuit):
-    def setup(self: Fibonacci):
-        self.a: Queriable = self.forward("a")
-        self.b: Queriable = self.forward("b")
+class FiboFirstStep(StepType):
+    def setup(self):
+        self.c = self.internal("c")
+        self.constr(eq(self.circuit.a, 1))
+        self.constr(eq(self.circuit.b, 1))
+        self.constr(eq(self.circuit.a + self.circuit.b, self.c))
+        self.transition(eq(self.circuit.b, self.circuit.a.next()))
+        self.transition(eq(self.c, self.circuit.b.next()))
+        self.transition(eq(self.circuit.n, self.circuit.n.next()))
 
-        self.fibo_step = self.step_type(FiboStep(self, "fibo_step"))
-        self.fibo_last_step = self.step_type(FiboLastStep(self, "fibo_last_step"))
-
-        self.pragma_first_step(self.fibo_step)
-        self.pragma_last_step(self.fibo_last_step)
-        self.pragma_num_steps(11)
-
-    def trace(self: Fibonacci, args: Any):
-        self.add(self.fibo_step, (1, 1))
-        a = 1
-        b = 2
-        for i in range(1, 10):
-            self.add(self.fibo_step, (a, b))
-            prev_a = a
-            a = b
-            b += prev_a
-        self.add(self.fibo_last_step, (a, b))
+    def wg(self, args):
+        a_value, b_value, n_value = args
+        self.assign(self.circuit.a, F(a_value))
+        self.assign(self.circuit.b, F(b_value))
+        self.assign(self.c, F(a_value + b_value))
+        self.assign(self.circuit.n, F(n_value))
 
 
 class FiboStep(StepType):
-    def setup(self: FiboStep):
+    def setup(self):
         self.c = self.internal("c")
         self.constr(eq(self.circuit.a + self.circuit.b, self.c))
         self.transition(eq(self.circuit.b, self.circuit.a.next()))
         self.transition(eq(self.c, self.circuit.b.next()))
+        self.transition(eq(self.circuit.n, self.circuit.n.next()))
 
-    def wg(self: FiboStep, args: Tuple[int, int]):
-        a_value, b_value = args
+    def wg(self, args):
+        a_value, b_value, n_value = args
         self.assign(self.circuit.a, F(a_value))
         self.assign(self.circuit.b, F(b_value))
         self.assign(self.c, F(a_value + b_value))
+        self.assign(self.circuit.n, F(n_value))
 
 
-class FiboLastStep(StepType):
-    def setup(self: FiboLastStep):
-        self.c = self.internal("c")
-        self.constr(eq(self.circuit.a + self.circuit.b, self.c))
+class Padding(StepType):
+    def setup(self):
+        self.transition(eq(self.circuit.b, self.circuit.b.next()))
+        self.transition(eq(self.circuit.n, self.circuit.n.next()))
 
-    def wg(self: FiboLastStep, values=Tuple[int, int]):
-        a_value, b_value = values
+    def wg(self, args):
+        a_value, b_value, n_value = args
         self.assign(self.circuit.a, F(a_value))
         self.assign(self.circuit.b, F(b_value))
-        self.assign(self.c, F(a_value + b_value))
+        self.assign(self.circuit.n, F(n_value))
+
+
+class Fibonacci(Circuit):
+    def setup(self):
+        self.a = self.forward("a")
+        self.b = self.forward("b")
+        self.n = self.forward("n")
+
+        self.fibo_first_step = self.step_type(FiboFirstStep(self, "fibo_first_step"))
+        self.fibo_step = self.step_type(FiboStep(self, "fibo_step"))
+        self.padding = self.step_type(Padding(self, "padding"))
+
+        self.pragma_num_steps(10)
+        self.pragma_first_step(self.fibo_first_step)
+        self.pragma_last_step(self.padding)
+
+        self.expose(self.b, Last())
+        self.expose(self.n, Last())
+
+    def trace(self, n):
+        self.add(self.fibo_first_step, (1, 1, n))
+        a = 1
+        b = 2
+        for i in range(1, n):
+            self.add(self.fibo_step, (a, b, n))
+            prev_a = a
+            a = b
+            b += prev_a
+        while self.needs_padding():
+            self.add(self.padding, (a, b, n))
 
 
 fibo = Fibonacci()
-fibo_witness = fibo.gen_witness(None)
+fibo_witness = fibo.gen_witness(7)
 fibo.halo2_mock_prover(fibo_witness)
+another_fibo_witness = fibo.gen_witness(4)
+fibo.halo2_mock_prover(another_fibo_witness)
