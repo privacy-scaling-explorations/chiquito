4f62866ebc2f8dd8d71a5b43f200cc8ae4b3c6b7
sandro <devspuig@pm.me>
2023-10-13 20:49:14 +0700

    refactor step types to more meaningful abstraction

--- examples/factorial.py
+++ examples/factorial.py
@@ -3,91 +3,98 @@ from chiquito.cb import eq
 from chiquito.util import F
 from chiquito.chiquito_ast import Last
 
-MAX_FACTORIAL = 100
+MAX_FACTORIAL = 10
+
+"""
+|    step_type      |  i  |  x   |
+----------------------------------
+|  setup_step       |  0  |  1   |
+|  operation_step   |  1  |  1   |
+|  operation_step   |  2  |  2   |
+|  operation_step   |  3  |  6   |
+|  result_step      |  4  |  24  |
+|  result_step      |  4  |  24  |
+|  result_step      |  4  |  24  |
+        ...
+"""
 
 
-class FactorialStep(StepType):
+class SetupStep(StepType):
     def setup(self):
-        # constrain the next value of x to be the product of the current x and the next i
-        self.transition(eq(self.circuit.x * (self.circuit.i + 1), self.circuit.x.next()))
-        # constrain the value of n to not change across iterations
-        self.transition(eq(self.circuit.n, self.circuit.n.next()))
+        # constrain `i` to zero
+        self.constr(eq(self.circuit.i, 0))
+        # constrain `x` to one
+        self.constr(eq(self.circuit.x, 1))
+        # constrain the next `x` to be equal to the current `x`
+        self.transition(eq(self.circuit.x, self.circuit.x.next()))
 
-    def wg(self, n_value, i_value, x_value):
-        self.assign(self.circuit.n, F(n_value))
+    def wg(self):
+        self.assign(self.circuit.i, F(0))
+        self.assign(self.circuit.x, F(1))
+
+
+class OperationStep(StepType):
+    def setup(self):
+        # constrain i.prev() + 1 == i
+        self.transition(eq(self.circuit.i.rot(-1) + 1, self.circuit.i))
+        # constrain i + 1 == i.next()
+        self.transition(eq(self.circuit.i + 1, self.circuit.i.next()))
+        # constrain the next `x` to be the product of the current `x` and the next `i`
+        self.transition(eq(self.circuit.x * (self.circuit.i + 1), self.circuit.x.next()))
+
+    def wg(self, i_value, x_value):
         self.assign(self.circuit.i, F(i_value))
         self.assign(self.circuit.x, F(x_value))
 
 
-class FactorialFinalStep(StepType):
+class ResultStep(StepType):
     def setup(self):
-        # constrain x to not change on the next step
+        # constrain `x` to not change
         self.transition(eq(self.circuit.x, self.circuit.x.next()))
-        # constrain n to not change on the next step
-        self.transition(eq(self.circuit.n, self.circuit.n.next()))
+        # constrain `i` to not change
+        self.transition(eq(self.circuit.i, self.circuit.i.next()))
 
-    def wg(self, n_value, x_value):
-        self.assign(self.circuit.n, F(n_value))
-        self.assign(self.circuit.x, F(x_value))
-
-
-class PaddingStep(StepType):
-    def setup(self):
-        # constrain x to not change on the next step
-        self.transition(eq(self.circuit.x, self.circuit.x.next()))
-        # constrain n to not change on the next step
-        self.transition(eq(self.circuit.n, self.circuit.n.next()))
-
-    def wg(self, n_value, x_value):
-        self.assign(self.circuit.n, F(n_value))
+    def wg(self, i_value, x_value):
+        self.assign(self.circuit.i, F(i_value))
         self.assign(self.circuit.x, F(x_value))
 
 
 class Factorial(Circuit):
     def setup(self):
-        # n will hold the requested factorial
-        self.n = self.forward("n")
-        # i will hold the current iteration on the computation
-        self.i = self.forward("i")
-        # x will hold the current result of the current factorial
+        # `i` holds the current iteration number
+        self.i = self.shared("i")
+        # `x` holds the current total result
         self.x = self.forward("x")
 
-        self.factorial_step = self.step_type(FactorialStep(self, "factorial_step"))
-        self.factorial_final_step = self.step_type(FactorialFinalStep(self, "factorial_final_step"))
-        self.padding_step = self.step_type(PaddingStep(self, "padding_step"))
+        self.setup_step = self.step_type(SetupStep(self, "setup_step"))
+        self.operation_step = self.step_type(OperationStep(self, "operation_step"))
+        self.result_step = self.step_type(ResultStep(self, "result_step"))
 
         self.pragma_num_steps(MAX_FACTORIAL + 1)
-        self.pragma_first_step(self.factorial_step)
-        self.pragma_last_step(self.padding_step)
+        self.pragma_first_step(self.setup_step)
+        self.pragma_last_step(self.result_step)
 
         self.expose(self.x, Last())
-        self.expose(self.n, Last())
+        self.expose(self.i, Last())
 
     def trace(self, n):
-        result = 1
-        for i in range(0, n + 1):
-            # on the first iteration, we don't want to bring the current result to zero
-            if i != 0:
-                result *= i
+        self.add(self.setup_step)
+        current_result = 1
 
-            # when we reach the last iteration
+        for i in range(1, n + 1):
+            current_result *= i
             if i == n:
-                if i == 0:
-                    # add a normal step if factorial of zero was requested
-                    # we need this because we constrain the first step to be of this type
-                    self.add(self.factorial_step, n, i, result)
-                else:
-                    # add the final type of step on other cases, to have different constraints on the step
-                    self.add(self.factorial_final_step, n, result)
+                # we found the result
+                self.add(self.result_step, i, current_result)
             else:
-                # if we are in a middle step, we keep adding the same type
-                self.add(self.factorial_step, n, i, result)
+                # more operations need to be done
+                self.add(self.operation_step, i, current_result)
 
-        # after all compute steps are added, fill with padding
         while self.needs_padding():
-            self.add(self.padding_step, n, result)
+            # if padding is needed, we propagate final values
+            self.add(self.result_step, n, current_result)
 
 
 factorial = Factorial()
-factorial_witness = factorial.gen_witness(0)
+factorial_witness = factorial.gen_witness(7)
 factorial.halo2_mock_prover(factorial_witness)
