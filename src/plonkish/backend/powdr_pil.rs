fd752e2d4e49cafa87cc7272812a0c8a6b768e84
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2023-10-16 15:18:42 +0700

    fully functional for fibo example

--- src/plonkish/backend/powdr_pil.rs
+++ src/plonkish/backend/powdr_pil.rs
@@ -35,9 +35,13 @@
 
 use crate::{
     ast::{Circuit, StepType},
-    wit_gen::TraceWitness, util::UUID,
+    util::UUID,
+    wit_gen::TraceWitness,
 };
-use std::fmt::{Debug, Write};
+use std::{
+    collections::HashMap,
+    fmt::{write, Debug, Write},
+};
 extern crate regex;
 
 pub struct ChiquitoPil<F, TraceArgs> {
@@ -53,64 +57,83 @@ impl<F: Debug, TraceArgs> ChiquitoPil<F, TraceArgs> {
 
 impl<F: Debug, TraceArgs> ChiquitoPil<F, TraceArgs> {
     pub fn to_pil(self: &ChiquitoPil<F, TraceArgs>) -> String {
-        // create annotation map for all signals and step types
-        // categorize annotations for signals and step types in UUID vectors
-        // note that internal signals defined in different step types will be duplicated
-        let mut annotations_map = self.ast.annotations.clone();
-        
-        let mut col_witness_uuids: Vec<UUID> = Vec::new();
-        for (_, step_type) in &self.ast.step_types {
-            annotations_map.extend(step_type.annotations.clone());
-            col_witness_uuids.extend::<Vec<UUID>>(step_type.signals.iter().map(|signal| signal.uuid()).collect());
-        }
-        for annotation in annotations_map.values_mut() {
-            *annotation = annotation.replace(" ", "_");
-        }
-
-        col_witness_uuids.extend::<Vec<UUID>>(self.ast.forward_signals.iter().map(|forward_signal| forward_signal.uuid()).collect());
-        col_witness_uuids.extend::<Vec<UUID>>(self.ast.shared_signals.iter().map(|forward_signal| forward_signal.uuid()).collect());
-
-        let mut col_fixed_uuids = self.ast.fixed_signals.iter().map(|fixed_signal| fixed_signal.uuid()).collect::<Vec<UUID>>();
-        let mut col_fixed_step_types_uuids: Vec<UUID> = self.ast.step_types.keys().cloned().collect();
-
-        assert!(col_witness_uuids.len() + col_fixed_uuids.len() + col_fixed_step_types_uuids.len() == annotations_map.len());
-
-        // declare witness columns
-        if !col_witness_uuids.is_empty() {
-            let mut col_witness = String::from("col witness ");
-            // get unique witness annotations
-            col_witness_uuids.iter().map(|uuid| annotations_map.get(&uuid).unwrap().clone()).collect::<Vec<String>>();
-
-        }
-
-
-        // declare non-step type fixed columns
-
-        // let mut col_witness = String::from("col witness ");
-        // for (_, step_type) in &self.ast.step_types {
-        //     for signal in &step_type.signals {
-        //         let annotation = annotations_map.get(&signal.uuid).unwrap();
-        //         if signal.is_fixed {
-        //             writeln!(col_witness, "col fixed {} = {}", signal_name, annotation);
-        //         } else {
-        //             writeln!(col_witness, "col witness {} = {}", signal_name, annotation);
-        //         }
-        //     }
-        // }
-
-        // let mut col_fixed = String::new();
-        // let mut col_fixed_step_types = String::new();
-
         // create new string buffer
         let mut pil = String::new();
         writeln!(pil, "constant %NUM_STEPS = {};", self.ast.num_steps);
         writeln!(pil, "namespace Circuit(%NUM_STEPS);");
 
-        // create fixed columns for step instance selections
-        // first, generate empty vectors for each step type
-        // for step_type in &self.ast.step_types {
+        // create annotation map for all signals and step types
+        // categorize annotations for signals and step types in UUID vectors
+        // note that internal signals defined in different step types will be duplicated
+        let mut annotations_map = self.ast.annotations.clone();
 
-        // }
+        let mut col_witness_uuids: Vec<UUID> = Vec::new();
+        for (_, step_type) in &self.ast.step_types {
+            annotations_map.extend(step_type.annotations.clone());
+            col_witness_uuids.extend::<Vec<UUID>>(
+                step_type
+                    .signals
+                    .iter()
+                    .map(|signal| signal.uuid())
+                    .collect(),
+            );
+        }
+        for annotation in annotations_map.values_mut() {
+            *annotation = annotation.replace(" ", "_");
+        }
+
+        col_witness_uuids.extend::<Vec<UUID>>(
+            self.ast
+                .forward_signals
+                .iter()
+                .map(|forward_signal| forward_signal.uuid())
+                .collect(),
+        );
+        col_witness_uuids.extend::<Vec<UUID>>(
+            self.ast
+                .shared_signals
+                .iter()
+                .map(|shared_signal| shared_signal.uuid())
+                .collect(),
+        );
+
+        let mut col_fixed_uuids = self
+            .ast
+            .fixed_signals
+            .iter()
+            .map(|fixed_signal| fixed_signal.uuid())
+            .collect::<Vec<UUID>>();
+        let mut col_fixed_step_types_uuids: Vec<UUID> =
+            self.ast.step_types.keys().cloned().collect();
+
+        assert!(
+            col_witness_uuids.len() + col_fixed_uuids.len() + col_fixed_step_types_uuids.len()
+                == annotations_map.len()
+        );
+
+        // declare witness columns
+        if !col_witness_uuids.is_empty() {
+            let mut col_witness = String::from("col witness ");
+            // get unique witness annotations
+            let col_witness_vars = col_witness_uuids
+                .iter()
+                .map(|uuid| annotations_map.get(&uuid).unwrap().clone())
+                .collect::<Vec<String>>();
+            col_witness = col_witness + col_witness_vars.join(", ").as_str() + ";";
+            writeln!(pil, "{}", col_witness);
+        }
+
+        // declare non-step type fixed columns
+        if !col_fixed_uuids.is_empty() {
+            let mut col_fixed = String::from("col fixed ");
+            // get unique witness annotations
+            let col_fixed_vars = col_fixed_uuids
+                .iter()
+                .map(|uuid| annotations_map.get(&uuid).unwrap().clone())
+                .collect::<Vec<String>>();
+            col_fixed = col_fixed + col_fixed_vars.join(", ").as_str() + ";";
+            writeln!(pil, "{}", col_fixed);
+        }
 
         // ISFIRST and ISLAST needed for pragma_first_step and pragma_last_step
         writeln!(
@@ -124,7 +147,7 @@ impl<F: Debug, TraceArgs> ChiquitoPil<F, TraceArgs> {
 
         // iterate over self.step_types
         for (_, step_type) in &self.ast.step_types {
-            let step_type_name = step_type.name.replace(" ", "_");
+            let step_type_name = annotations_map.get(&step_type.uuid()).unwrap();
             writeln!(pil, "// == step type {} start ==", step_type_name).unwrap();
             let mut step_type_selector = String::new();
             write!(step_type_selector, "col fixed {} = [", step_type_name);
@@ -142,28 +165,26 @@ impl<F: Debug, TraceArgs> ChiquitoPil<F, TraceArgs> {
                 // println!("step instance: {:?}", step_instance.step_type_uuid);
             }
             writeln!(pil, "{}", step_type_selector);
-            writeln!(pil, "{}", step_type.to_pil().as_str());
+            let is_last_step_instance =
+                step_type.uuid() == self.witness.step_instances.last().unwrap().step_type_uuid;
+            writeln!(
+                pil,
+                "{}",
+                step_type
+                    .to_pil(&annotations_map, is_last_step_instance)
+                    .as_str()
+            );
         }
 
         // pragma_first_step
         if let Some(first_step) = self.ast.first_step {
-            let first_step_name = self
-                .ast
-                .annotations
-                .get(&first_step)
-                .unwrap()
-                .replace(" ", "_");
+            let first_step_name = annotations_map.get(&first_step).unwrap();
             writeln!(pil, "ISFIRST * (1 - {}) = 0", first_step_name);
         }
 
         // pragma_last_step
         if let Some(last_step) = self.ast.last_step {
-            let last_step_name = self
-                .ast
-                .annotations
-                .get(&last_step)
-                .unwrap()
-                .replace(" ", "_");
+            let last_step_name = annotations_map.get(&last_step).unwrap();
             writeln!(pil, "ISLAST * (1 - {}) = 0", last_step_name);
         }
 
@@ -171,24 +192,28 @@ impl<F: Debug, TraceArgs> ChiquitoPil<F, TraceArgs> {
     }
 }
 
-// impl<F: Debug, TraceArgs> Circuit<F, TraceArgs> {
-
-// }
-
 impl<F: Debug> StepType<F> {
-    pub fn to_pil(self: &StepType<F>) -> String {
+    pub fn to_pil(
+        self: &StepType<F>,
+        annotations_map: &HashMap<UUID, String>,
+        is_last_step_instance: bool,
+    ) -> String {
         let mut pil = String::new();
-        let step_type_name = &self.name.replace(" ", "_");
+        let step_type_name = annotations_map.get(&self.uuid()).unwrap();
         for constraint in &self.constraints {
             let expr = convert_to_pil_rotation(&format!("{:?}", constraint.expr));
 
-            writeln!(pil, "{} * {} = 0", step_type_name, expr);
+            writeln!(pil, "{} * {} = 0;", step_type_name, expr);
         }
         for transition in &self.transition_constraints {
             // apply convert_to_pil_rotation to the Debug string of transition.expr
             let expr = convert_to_pil_rotation(&format!("{:?}", transition.expr));
-
-            writeln!(pil, "{} * {} = 0", step_type_name, expr);
+            // disable transition constraint in the last step instance
+            if (is_last_step_instance) {
+                writeln!(pil, "(1 - ISLAST) * {} * {} = 0;", step_type_name, expr);
+            } else {
+                writeln!(pil, "{} * {} = 0;", step_type_name, expr);
+            }
         }
         pil
     }
