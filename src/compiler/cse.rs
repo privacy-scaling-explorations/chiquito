e9553d1295fb88d3a37a66bd76107da1d82f4dae
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2024-07-24 12:33:37 +0100

    run expression replacements one common expression at a time

--- src/compiler/cse.rs
+++ src/compiler/cse.rs
@@ -7,23 +7,32 @@ use crate::{
 use rand_chacha::{rand_core::SeedableRng, ChaCha20Rng};
 use std::{collections::HashMap, fmt::Debug, hash::Hash, marker::PhantomData};
 
+type HashedExpr<F> = Expr<F, Queriable<F>, HashResult>;
+
 pub(super) fn cse<F: Field + Hash>(
     mut circuit: SBPIR<F, NullTraceGenerator>,
 ) -> SBPIR<F, NullTraceGenerator> {
-    // Create dummy assignments
-    let mut queriables: Vec<Queriable<F>> = Vec::new();
-    for signal in circuit.forward_signals.iter() {
-        queriables.push(Queriable::Forward(*signal, false));
-        queriables.push(Queriable::Forward(*signal, true));
-    }
+    let mut queriables = collect_forward_signals(&circuit);
 
-    // For each step type run a CSE pass
     for (_, step_type) in circuit.step_types.iter_mut() {
         let mut cse = CommonSubexpressionEliminator::new();
         cse.run(step_type, &mut queriables);
     }
 
     circuit
+}
+
+fn collect_forward_signals<F: Field>(circuit: &SBPIR<F, NullTraceGenerator>) -> Vec<Queriable<F>> {
+    circuit
+        .forward_signals
+        .iter()
+        .flat_map(|signal| {
+            vec![
+                Queriable::Forward(*signal, false),
+                Queriable::Forward(*signal, true),
+            ]
+        })
+        .collect()
 }
 
 #[derive(Debug, Clone)]
@@ -42,46 +51,17 @@ impl Default for Config {
 }
 
 struct CommonSubexpressionEliminator<F> {
-    common_exprs: Vec<Expr<F, Queriable<F>, HashResult>>,
-
-    hashed_exprs_map: HashMap<u64, Expr<F, Queriable<F>, HashResult>>,
+    initial_pass: bool,
+    common_exprs: Vec<HashedExpr<F>>,
+    hashed_exprs_map: HashMap<u64, HashedExpr<F>>,
     count_map: HashMap<u64, u32>,
-
     config: Config,
 }
 
 impl<F: Field + Hash> CommonSubexpressionEliminator<F> {
-    pub(super) fn run(&mut self, step: &mut StepType<F, ()>, queriables: &mut Vec<Queriable<F>>) {
-        // Collect internal queriables from the step type and add them to the list of queriables
-        step.signals.iter().for_each(|signal| {
-            queriables.push(Queriable::Internal(*signal));
-        });
-
-        // Generate random assignments for the queriables
-        let mut rng = ChaCha20Rng::seed_from_u64(0);
-        let random_assignments: VarAssignments<F, Queriable<F>> = queriables
-            .iter()
-            .cloned()
-            .map(|q| (q, F::random(&mut rng)))
-            .collect();
-
-        // Convert StepType<F, ()> to StepType<F, HashResult>
-        let mut hashed_step = step.with_meta::<HashResult>(&random_assignments);
-
-        // Collect and analyze all subexpressions
-        self.collect_all_subexpressions(&hashed_step);
-        self.identify_common_subexpressions();
-
-        let mut signal_factory = SignalFactory::default();
-        hashed_step
-            .decomp_constraints(|expr| replace_expr(expr, &self.common_exprs, &mut signal_factory));
-
-        // Convert back to StepType<F, ()> and update the original step
-        *step = hashed_step.without_meta();
-    }
-
     fn new() -> Self {
         CommonSubexpressionEliminator {
+            initial_pass: true,
             common_exprs: Vec::new(),
             hashed_exprs_map: HashMap::new(),
             count_map: HashMap::new(),
@@ -89,20 +69,55 @@ impl<F: Field + Hash> CommonSubexpressionEliminator<F> {
         }
     }
 
+    pub(super) fn run(&mut self, step: &mut StepType<F, ()>, queriables: &mut Vec<Queriable<F>>) {
+        while !self.common_exprs.is_empty() || self.initial_pass {
+            self.initial_pass = false;
+            self.process_step(step, queriables);
+        }
+    }
+
+    fn process_step(&mut self, step: &mut StepType<F, ()>, queriables: &mut Vec<Queriable<F>>) {
+        self.add_internal_queriables(step, queriables);
+        let random_assignments = Self::generate_random_assignments(queriables);
+        let mut hashed_step = step.with_meta::<HashResult>(&random_assignments);
+
+        self.collect_all_subexpressions(&hashed_step);
+        self.identify_common_subexpressions();
+
+        if self.common_exprs.is_empty() {
+            return;
+        }
+
+        self.replace_common_subexpressions(&mut hashed_step);
+        self.reset_state();
+
+        *step = hashed_step.without_meta();
+    }
+
+    fn add_internal_queriables(&self, step: &StepType<F, ()>, queriables: &mut Vec<Queriable<F>>) {
+        queriables.extend(step.signals.iter().map(|&signal| Queriable::Internal(signal)));
+    }
+
+    fn generate_random_assignments(queriables: &[Queriable<F>]) -> VarAssignments<F, Queriable<F>> {
+        let mut rng = ChaCha20Rng::seed_from_u64(0);
+        queriables
+            .iter()
+            .cloned()
+            .map(|q| (q, F::random(&mut rng)))
+            .collect()
+    }
+
     fn collect_all_subexpressions(&mut self, step: &StepType<F, HashResult>) {
         let mut all_subexpressions = Vec::new();
 
-        // Collect subexpressions from constraints
         for constraint in &step.constraints {
             Self::collect_subexpressions(&constraint.expr, &mut all_subexpressions);
         }
 
-        // Collect subexpressions from transition constraints
         for constraint in &step.transition_constraints {
             Self::collect_subexpressions(&constraint.expr, &mut all_subexpressions);
         }
 
-        // Count occurrences of each subexpression
         for expr in all_subexpressions {
             let hash = expr.meta().hash;
             *self.count_map.entry(hash).or_insert(0) += 1;
@@ -111,25 +126,19 @@ impl<F: Field + Hash> CommonSubexpressionEliminator<F> {
     }
 
     fn collect_subexpressions(
-        expr: &Expr<F, Queriable<F>, HashResult>,
-        subexpressions: &mut Vec<Expr<F, Queriable<F>, HashResult>>,
+        expr: &HashedExpr<F>,
+        subexpressions: &mut Vec<HashedExpr<F>>,
     ) {
-        // Add the current expression to the list of subexpressions
         subexpressions.push(expr.clone());
 
-        // Recursively collect subexpressions
         match expr {
             Expr::Sum(ses, _) | Expr::Mul(ses, _) => {
-                for se in ses {
-                    Self::collect_subexpressions(se, subexpressions);
-                }
+                ses.iter().for_each(|se| Self::collect_subexpressions(se, subexpressions));
             }
             Expr::Neg(se, _) | Expr::Pow(se, _, _) | Expr::MI(se, _) => {
                 Self::collect_subexpressions(se, subexpressions);
             }
-            Expr::Const(_, _) | Expr::Query(_, _) | Expr::Halo2Expr(_, _) => {
-                // These are leaf nodes, so we don't need to collect subexpressions
-            }
+            Expr::Const(_, _) | Expr::Query(_, _) | Expr::Halo2Expr(_, _) => {}
         }
     }
 
@@ -138,18 +147,44 @@ impl<F: Field + Hash> CommonSubexpressionEliminator<F> {
             .hashed_exprs_map
             .iter()
             .filter_map(|(&hash, expr)| {
-                if self.count_map.get(&hash).copied().unwrap_or(0)
-                    >= self.config.min_occurrences.unwrap_or(2) as u32
-                    && expr.degree() >= self.config.min_degree.unwrap_or(2)
-                    && !matches!(expr, Expr::MI(_, _))
-                // Exclude MI expressions
-                {
+                let count = self.count_map.get(&hash).copied().unwrap_or(0);
+                let min_occurrences = self.config.min_occurrences.unwrap_or(2) as u32;
+                let min_degree = self.config.min_degree.unwrap_or(2);
+
+                if count >= min_occurrences && expr.degree() >= min_degree && !matches!(expr, Expr::MI(_, _)) {
                     Some(expr.clone())
                 } else {
                     None
                 }
             })
             .collect();
+
+        // sort by degree and number of occurrences - this allows us to replace the best common subexpression
+        self.common_exprs.sort_by(|a, b| {
+            let a_degree = a.degree();
+            let b_degree = b.degree();
+            let a_occurrences = self.count_map.get(&a.meta().hash).copied().unwrap_or(0);
+            let b_occurrences = self.count_map.get(&b.meta().hash).copied().unwrap_or(0);
+
+            b_degree
+                .cmp(&a_degree)
+                .then(b_occurrences.cmp(&a_occurrences))
+        });
+    }
+
+    fn replace_common_subexpressions(&self, hashed_step: &mut StepType<F, HashResult>) {
+        let mut signal_factory = SignalFactory::default();
+        // we should only replace the best common subexpression and then analyze everything again
+        // to find the next best common subexpression
+        if let Some(best_expr) = self.common_exprs.first() {
+            hashed_step.decomp_constraints(|se| replace_expr(se, best_expr, &mut signal_factory));
+        }
+    }
+
+    fn reset_state(&mut self) {
+        self.count_map.clear();
+        self.hashed_exprs_map.clear();
+        self.common_exprs.clear();
     }
 }
 
@@ -166,4 +201,48 @@ impl<F> poly::SignalFactory<Queriable<F>> for SignalFactory<F> {
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use std::vec;
+
+    use halo2_proofs::halo2curves::bn256::Fr;
+
+    use crate::{poly::{HashResult, ToExpr}, sbpir::{query::Queriable, InternalSignal}};
+
+    use super::CommonSubexpressionEliminator;
+
+
+
+    #[test]
+    fn test_find_common_expressions() {
+        let a: Queriable<Fr> = Queriable::Internal(InternalSignal::new("a"));
+        let b = Queriable::Internal(InternalSignal::new("b"));
+        let c = Queriable::Internal(InternalSignal::new("c"));
+        let d = Queriable::Internal(InternalSignal::new("d"));
+
+        let vars = vec![a.clone(), b.clone(), c.clone(), d.clone()];
+
+        let expr1 = a * b + c;
+        let expr2 = d * a * b;
+        let expr3 = 1.expr() + a * b + c;
+
+        let cse = CommonSubexpressionEliminator::<Fr>::new();
+
+        let random_assignments = CommonSubexpressionEliminator::generate_random_assignments(&vars);
+
+        let mut hashed_expr1 = expr1.with_meta::<HashResult>(&random_assignments);
+        let mut hashed_expr2 = expr2.with_meta::<HashResult>(&random_assignments);
+        let mut hashed_expr3 = expr3.with_meta::<HashResult>(&random_assignments);
+
+        
+
+    
+    }
+}
+
+// For each step type:
+//  get common subexpressions (sort by degree and num_occurr)
+//  find the best expression to replace (bigger degree and occurrences)
+//  create a new auto_signal with the expression
+//  find all the occurrences of the
+
+// do everything again until not common subexpressions
